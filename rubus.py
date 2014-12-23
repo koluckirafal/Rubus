39450a6061a769129326571ae6f140118cf1e469
Rafał Kołucki <BluRafPL@gmail.com>
2014-12-23 17:20:23 +0100

    More PEP8 compliant code

--- rubus.py
+++ rubus.py
@@ -25,27 +25,27 @@ import urllib.request
 from subprocess import Popen, PIPE, STDOUT
 
 
-shortID='mcpi'
-longID='minecraft-pi'
+shortID = 'mcpi'
+longID = 'minecraft-pi'
 
-gameVersion='0.1.1'
-#gameVersion='0.1'   #if you want to download leaked version
+gameVersion = '0.1.1'
+#gameVersion = '0.1'   #if you want to download leaked version
 
 #gameVersion >= 0.1.1
-remotePool='https://s3.amazonaws.com/assets.minecraft.net/pi/'            
+remotePool = 'https://s3.amazonaws.com/assets.minecraft.net/pi/'
 #gameVersion == 0.1.0
-#remotePool='http://technowiz2cubed.weebly.com/uploads/1/6/8/9/16891832/'
+#remotePool = 'http://technowiz2cubed.weebly.com/uploads/1/6/8/9/16891832/'
 
-envPool=os.path.join(os.path.expanduser('~'), '.' + shortID)
-localPool=os.path.join(envPool, 'packages')
-gamePool=os.path.join(envPool, 'versions')
+envPool = os.path.join(os.path.expanduser('~'), '.' + shortID)
+localPool = os.path.join(envPool, 'packages')
+gamePool = os.path.join(envPool, 'versions')
 
-intro='''\n    Rubus Launcher for Minecraft: Pi Edition, 
+intro = '''\n    Rubus Launcher for Minecraft: Pi Edition,
     copyright (C) Rafał 'BluRaf' Kołucki, 2014\n\
-    This program comes with ABSOLUTELY NO WARRANTY; 
+    This program comes with ABSOLUTELY NO WARRANTY;
     for details press 'License' button in 'About' card.\n\
     This is free software, and you are welcome to redistribute it\n\
-    under certain conditions; 
+    under certain conditions;
     press 'License' button in 'About' card for details.\n'''
 
 
@@ -68,6 +68,7 @@ def checkLocalPool(localPool):
             print('- making local repository directory...')
             os.mkdir(localPool)
 
+
 def checkGamePool(gamePool):
     while True:
         if os.path.isdir(gamePool) == 1:
@@ -79,19 +80,24 @@ def checkGamePool(gamePool):
 
 
 def download(longID, gameVersion, remotePool, localPool):
-    remotePackage=urllib.request.urlopen('%s%s-%s.tar.gz' % (remotePool, longID, gameVersion))
-    localPackage=open(os.path.join(localPool, '%s-%s.tar.gz' % (longID, gameVersion)), 'wb')
+    remotePackage = urllib.request.urlopen('%s%s-%s.tar.gz'
+                                           % (remotePool, longID, gameVersion))
+    localPackage = open(os.path.join(localPool, '%s-%s.tar.gz'
+                                     % (longID, gameVersion)), 'wb')
     localPackage.write(remotePackage.read())
     localPackage.close()
 
+
 def unpack(longID, gameVersion, localPool, gamePool):
-    localPackage=tarfile.open(os.path.join(localPool, \
-					   '%s-%s.tar.gz' % (longID, gameVersion)), "r:gz")
+    localPackage = tarfile.open(os.path.join(localPool, \
+                                           '%s-%s.tar.gz' \
+                                           % (longID, gameVersion)), "r:gz")
     localPackage.extractall(gamePool)
     localPackage.close()
 
 
-def prepareGameInstance(shortID, longID, gameVersion, gamePool, remotePool, localPool):
+def prepareGameInstance(shortID, longID, gameVersion, \
+                        gamePool, remotePool, localPool):
     if os.path.isdir(os.path.join(gamePool, gameVersion)) == 1:
         print('Switching to unpacked game directory, ...')
         os.chdir(os.path.join(gamePool, gameVersion))
@@ -108,15 +114,17 @@ def prepareGameInstance(shortID, longID, gameVersion, gamePool, remotePool, loca
             unpack(longID, gameVersion, localPool, gamePool)
             print('Game package unpacked!')
         print('Renaming version directory...')
-        os.rename(os.path.join(gamePool, shortID), os.path.join(gamePool, gameVersion))
-        prepareGameInstance(shortID, longID, gameVersion, gamePool, remotePool, localPool)
+        os.rename(os.path.join(gamePool, shortID), \
+                  os.path.join(gamePool, gameVersion))
+        prepareGameInstance(shortID, longID, gameVersion, \
+                            gamePool, remotePool, localPool)
 
 
 def runBinary(binaryPath, binaryName):
     Popen(['file', os.path.join(binaryPath, binaryName)])
     Popen(os.path.join(binaryPath, binaryName))
-    
-    
+
+
 def main():
     print(intro)
     print('Checking for local repository:')
@@ -124,8 +132,9 @@ def main():
     print('Checking for game directories:')
     checkGamePool(gamePool)
 
-    #prepare 
-    prepareGameInstance(shortID, longID, gameVersion, gamePool, remotePool, localPool)
+    #prepare
+    prepareGameInstance(shortID, longID, gameVersion, \
+                        gamePool, remotePool, localPool)
 
     #here launch the game
     runBinary(os.path.join(gamePool, gameVersion), longID)
